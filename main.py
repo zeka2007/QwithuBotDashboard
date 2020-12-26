4ee99506aca16eb2df328b61405d6b20296113b8
zeka2007 <75169925+zeka2007@users.noreply.github.com>
2020-12-26 16:21:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ def exit():
     resp.set_cookie('token', '0', max_age=0)
     return resp
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='127.0.0.1', port=5000)
