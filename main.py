3c58985ca3eeb980e24dcf9ef5bcd839b48f60c5
zeka2007 <75169925+zeka2007@users.noreply.github.com>
2021-01-10 19:41:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for, make_response
 from oauth import Oauth
-
+import json
 app = Flask(__name__)
 
 
@@ -35,6 +35,8 @@ def index():
         return render_template("index.html", user_btn_url = url, articles = user_name, avatar = user_avatar, bot_redirect = Oauth.discord_bot_oauth_url)
 @app.route('/login', methods = ['get'])
 def login():
+    global user_name
+    global user_avatar
     try:
         if request.cookies.get('token') == None:
             print("bub")
@@ -62,10 +64,101 @@ def login():
     except Exception as e:
         print(e)
         return redirect(Oauth.discord_login_url)
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", articles = user_name, avatar = user_avatar, bot_redirect = Oauth.discord_bot_oauth_url)
+@app.route('/servers')
+def servers():
+    if request.cookies.get('token') == None:
+        return redirect(Oauth.discord_login_url)
+    else:
+        global members_json
+        global user
+        members = []
+        guilds = []
+        guilds_icon = []
+        members_list = []
+        counter = [0]
+
+        at = request.cookies.get('token')
+
+        user_json = Oauth.get_user_json(at)
+        user_name = user_json.get("username")
+        user_avatar = f'https://cdn.discordapp.com/avatars/{user_json.get("id")}/{user_json.get("avatar")}.png?size=1024'
+        guilds_json = Oauth.get_guilds_json(at)
+        global members_json
+        for guild in guilds_json:
+
+            if guild.get("permissions") & 0x8 == 0x8:
+                if Oauth.get_guild_members_json(guild.get("id"), Oauth.client_id) == {'message': 'Missing Access', 'code': 50001}:
+                    g = {
+                    'name': guild.get("name"),
+                    'id': guild.get("id"),
+                    'icon': guild.get("icon"),
+                    'redirect': f'{Oauth.discord_bot_oauth_url}&guild_id={guild.get("id")}',
+                    'bot_in': False
+                    }
+                    guilds.append(g)
+                else:
+                    g = {
+                    'name': guild.get("name"),
+                    'id': guild.get("id"),
+                    'icon': guild.get("icon"),
+                    'redirect': f'/{guild.get("id")}',
+                    'bot_in': True
+                    }
+                    guilds.append(g)
+        # return str(guilds)
+        return render_template("server.html", servers = guilds, articles = user_name, avatar = user_avatar, bot_redirect = Oauth.discord_bot_oauth_url,
+        url = Oauth.discord_bot_oauth_url)
+
+@app.route("/<int:server_id>")
+def server_settings(server_id):
+
+    if request.cookies.get('token') == None:
+        return redirect(Oauth.discord_login_url)
+    else:
+        at = request.cookies.get('token')
+
+        user_json = Oauth.get_user_json(at)
+        user_name = user_json.get("username")
+        user_avatar = f'https://cdn.discordapp.com/avatars/{user_json.get("id")}/{user_json.get("avatar")}.png?size=1024'
+        guilds_json = Oauth.get_guild_by_id_json(server_id)
+        # Oauth.send_message(766620925981753364) # отправка сообщений
+        return render_template("dashboard.html", articles = user_name, avatar = user_avatar, bot_redirect = Oauth.discord_bot_oauth_url,
+        server_id = server_id)
+@app.route("/<int:server_id>/embed_message",  methods = ['POST', 'GET'])
+def embed_message(server_id):
+        if request.cookies.get('token') == None:
+            return redirect(Oauth.discord_login_url)
+        else:
+            channels_json = Oauth.get_guild_channels_json(server_id)
+
+            at = request.cookies.get('token')
+
+            user_json = Oauth.get_user_json(at)
+            user_name = user_json.get("username")
+            user_avatar = f'https://cdn.discordapp.com/avatars/{user_json.get("id")}/{user_json.get("avatar")}.png?size=1024'
+
+        if request.method == 'POST':
+            text  = request.form['message']
+            choose = request.form['channels']
+            title = request.form['title']
+            Oauth.send_message(choose, title, text)
+            return render_template("embeds.html", articles = user_name, avatar = user_avatar,
+            channels = channels_json, bot_redirect = Oauth.discord_bot_oauth_url)
+
+        else:
+
+                return render_template("embeds.html", articles = user_name, avatar = user_avatar,
+                server_id = server_id,
+                channels = channels_json, bot_redirect = Oauth.discord_bot_oauth_url)
 @app.route('/exit')
 def exit():
     resp = make_response(redirect(url_for('index')))
     resp.set_cookie('token', '0', max_age=0)
     return resp
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug = True)
+client.run('NzY4MTQ4NjA0MzY3NTM2MTM4.X48QIg.FfL0oSJUx7BV1KUi6FpwFXgyW9E')
--- oauth.py
+++ oauth.py
@@ -1,17 +1,20 @@
 import requests
-
+import json
+import os
 class Oauth:
+    bot_token = os.environ.get('BOT_TOKEN')
     client_id = "768148604367536138"
-    client_secret = "mSH2k8pwVI5Gm4cPkAPbzYnIcFlwWCGO"
-    redirect_uri = "https://qwithubotdashboard.zeet.app/login"
+    client_secret = os.environ.get('CLIENT_SECRET') 
+    redirect_uri = "http://127.0.0.1:5000/login"
     scope = "identify%20guilds%20guilds.join"
     discord_login_url = f"https://discord.com/api/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scope}"
     discord_token_url = "https://discord.com/api/oauth2/token"
-    discord_api_url = "https://discord.com/api"
+    discord_api_url = "https://discord.com/api/v6"
 
     discord_bot_redirect_uri = "http%3A%2F%2F127.0.0.1%3A5000%2Flogin"
     discord_bot_scope = "8"
     discord_bot_oauth_url = f'https://discord.com/api/oauth2/authorize?client_id={client_id}&permissions={discord_bot_scope}&redirect_uri={discord_bot_redirect_uri}&scope=bot'
+    # &guild_id=775650720761774111
 
     @staticmethod
     def get_access_token(code):
@@ -40,3 +43,39 @@ class Oauth:
 
         guilds_object = requests.get(url = url, headers = headers).json()
         return guilds_object
+    @staticmethod
+    def get_guild_members_json(guild_id, user_id):
+        url = f"{Oauth.discord_api_url}/guilds/{guild_id}/members/{user_id}"
+        headers = {"Authorization": f"Bot {Oauth.bot_token}"}
+
+        member = requests.get(url = url, headers = headers).json()
+        return member
+    @staticmethod
+    def get_guild_by_id_json(guild_id):
+        url = f"{Oauth.discord_api_url}/guilds/{guild_id}"
+        headers = {"Authorization": f"Bot {Oauth.bot_token}"}
+
+        guild = requests.get(url = url, headers = headers).json()
+        return guild
+
+    @staticmethod
+    def get_guild_channels_json(guild_id):
+        url = f"{Oauth.discord_api_url}/guilds/{guild_id}/channels"
+        headers = {"Authorization": f"Bot {Oauth.bot_token}"}
+
+        channels = requests.get(url = url, headers = headers).json()
+        return channels
+
+    @staticmethod
+    def send_message(channel_id, title, message):
+        url = f"{Oauth.discord_api_url}/channels/{channel_id}/messages"
+        headers = {'content-type': 'application/json',
+                    "Authorization": f"Bot {Oauth.bot_token}"}
+            "embed": {
+                "color": 0x0c0c0,
+                "title": title,
+                "description": message
+            }
+        }
+
+        message = requests.post(url = url, data=json.dumps(send), headers = headers)
